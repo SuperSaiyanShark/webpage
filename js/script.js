d6e50b7a623b883e2189b3acab43eee06f57f78b
SuperSaiyanShark <lonielrana@gmail.com>
2026-02-19 11:38:10 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
 console.log("js/script.js")
 
-const submitBtn = document.getElementById("js/script.js");
+const submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click",function(event){
 
     event.preventDefault();
     alert("Thank you! Your message has been received!")
 });
+
+const bgthemeToggle = document.getElementById("themeToggle");
+
+
+themeToggle.addEventListener("click",function(){
+
+    document.body.classList.toggle("dark-mode-bg");
+});
+
+const toggleSkills = document.getElementById("toggleSkills");
+const skillsSection = document.getElementById("skillsSection");
+
+toggleSkills.addEventListener("click",function()
+{
+    skillsSection.classList.toggle("hidden")
+})
+
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    if (name===""||email===""){
+        alert("Please fill in all required fields.");
+    }else{
+        alert("Form submitted successfully!");
+    }
+});
+
